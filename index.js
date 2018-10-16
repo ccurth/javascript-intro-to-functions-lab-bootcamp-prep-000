7ca406f5a13bdc36f91ea26295dfe0200cd5c6de
thoughtful-calculator-1463 <ccurth@gmail.com>
2018-10-16 06:10:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ function logWhisper(string) {
   console.log(`hello`)
 }
 
-function sayHiToGrandma(string) {
-  return string()
+function sayHiToGrandma(string){
+  if (string === string.toLowerCase()) return "I can't hear you!";
+  else if (string === "I love you, Grandma.") return "I love you, too.";
+  else if (string === string.toUpperCase()) return "YES INDEED!";
 }
